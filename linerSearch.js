4adb9c06eaa638db398f59425c26903c3ecd6dac
Nick <k.kolabis@mail.ru>
2023-02-09 16:47:52 +0300

    liner search algorithm

--- linerSearch.js
+++ linerSearch.js
@@ -1,7 +1,13 @@
-const array = []
+const array = [1, 10, 12, 13, 14, 15, 17, 19, 3, 5, 6, 9]
 
-for (let i = 0; i < 20; i++) {
-    array.push(Math.round(Math.random() * 20))
+
+function linerSearch(array, target) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === target) {
+            return i
+        }
+    }
+    return -1
 }
 
-console.log(array)
+console.log(linerSearch(array, 14))
